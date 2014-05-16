26c3bef70b9e8304ef3904401334d9d65d4fde2c
zinedine <zinouh@filewave.com>
2014-05-16 17:05:14 -0400

    SN-0002 - added commandl ine arguments basic code

--- synapse.cpp
+++ synapse.cpp
@@ -1,7 +1,16 @@
-#include<iostream.h>
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
 
-int main(int, char **) {
+int main(int argc, char * args[]) {
+  /* main program*/
 
+  cout << "The arguments to " << pszArgs[0] <<  "are:\n";
+  for (int i = 1; i < nNumberofArgs; i++)
+  {
+         cout << i << ":" << pszArgs[i] << "\n";
+  }
 
+  system("pause");
   return 0;
 }
